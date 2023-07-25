4b8c628cc9ba840b0250fe97b075e63db46c8fde
Nick Morrissey <morrissey.nicholas@gmail.com>
2023-07-25 17:34:43 +1000

    fixed dynamic bug

--- app/api/db/listings/route.ts
+++ app/api/db/listings/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { getAllListings } from "../requests";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   return getAllListings()
     .then((res) => res)
